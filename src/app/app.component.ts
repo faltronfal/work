6356f94eb621d1b9382e2cdc26b65008fa5332ae
Admin <Admin@example.com>
2018-07-17 11:28:56 +0300

    add first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<tree-root [focused]="true" [nodes]="nodes"></tree-root>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+ 
+
+  nodes = [
+    {
+      id: 1,
+      name: "Friends",
+      type: "Group",
+      children: [
+        {id: 2, name: "Udi", type: "Contact"},
+        {id: 3, name: "Tommy", type: "Contact"},
+        {
+          id: 6,
+          name: "Old Friends",
+          type: "Group",
+          children: [
+            {id: 7, name: "Itay", type: "Contact"},
+          ]
+        },
+      ]
+    },
+    {
+      id: 4,
+      name: "Family",
+      type: "Group",
+      children: [
+        {id: 5, name: "Roni", type: "Contact"},
+      ]
+
+    },
+    {id: 8, name: "Ori", type: "Contact"},
+
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { TreeModule } from 'angular-tree-component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
